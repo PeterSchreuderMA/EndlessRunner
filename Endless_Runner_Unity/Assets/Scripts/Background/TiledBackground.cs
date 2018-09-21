6bc6a4a909321a369e0d66acef816e6d446cdf51
PeterSchreuderMA <24441@ma-web.nl>
2018-09-21 21:24:52 +0200

    Commit 01

---
+++ Endless_Runner_Unity/Assets/Scripts/Background/TiledBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour {
+
+    public int textureSize = 32;
+
+    public bool scaleH = true;
+    public bool scaleV = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        var newWidth = !scaleH ? 1 : Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale));
+        var newHeight = !scaleV ? 1 : Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale));
+
+        transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1);
+
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
+    }
+}
--- Endless_Runner_Unity/Assets/Scripts/Camera/PixelPerfectCamera.cs
+++ Endless_Runner_Unity/Assets/Scripts/Camera/PixelPerfectCamera.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PixelPerfectCamera : MonoBehaviour {
+public class PixelPerfectCamera : MonoBehaviour
+{
+
+    public static float pixelsToUnits = 1f;
+    public static float scale = 1f;
+
+    public Vector2 nativeResolution = new Vector2(240, 160);
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+    {
+        var camera = GetComponent<Camera>();
+
+        if (camera.orthographic)
+        {
+            scale = Screen.height / nativeResolution.y;
+            pixelsToUnits *= scale;
+            camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
